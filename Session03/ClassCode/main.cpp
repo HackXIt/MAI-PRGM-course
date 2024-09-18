e4b8e447f7ebc69b701c23a38f1a2f56dd222f3d
HackXIt <hackxit@gmail.com>
2024-09-18 19:01:07 +0200

    Add class code (Session03)

---
+++ Session03/ClassCode/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    Person p1("Max");
+    Person p2 = p1;
+
+    std::cout << "Name: " << p2.getName() << std::endl;
+
+    Student s1("Tom", "Math");
+    Student s2 = s1;
+    std::cout << "Name: " << s2.getName() << std::endl;
+
+    Person* studentPtr = new Student("John", "Math");
+    Person* studentPtr2 = studentPtr;
+
+
+    return 0;
+}
